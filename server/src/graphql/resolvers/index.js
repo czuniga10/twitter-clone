c082812961d2e83f1cb43d536b7508f08689f640
Chad <chad.zuniga10@gmail.com>
2018-04-20 18:52:20 -0600

    backend mostly done

--- server/src/graphql/resolvers/index.js
+++ server/src/graphql/resolvers/index.js
@@ -2,12 +2,14 @@ import GraphQLDate from 'graphql-date';
 
 import TweetResolvers from './tweet-resolvers';
 import UserResolvers from './user-resolvers';
+import userResolvers from './user-resolvers';
 
 export default {
   Date: GraphQLDate,
   Query: {
     getTweet: TweetResolvers.getTweet,
     getTweets: TweetResolvers.getTweets,
+    me: userResolvers.me,
   },
   Mutation: {
     createTweet: TweetResolvers.createTweet,
--- server/src/graphql/schema.js
+++ server/src/graphql/schema.js
@@ -3,7 +3,20 @@ scalar Date
 type Status {
   message: String!
 }
+type Auth {
+  token: String
+}
 type User {
+  _id: ID!
+  username: String
+  email: String!
+  firstName: String
+  lastName: String
+  avatar: String
+  createdAt: Date!
+  updatedAt: Date!
+}
+type Me {
   _id: ID!
   username: String
   email: String!
@@ -22,13 +35,14 @@ type Tweet {
 type Query {
   getTweet(_id: ID!): Tweet
   getTweets: [Tweet]
+  me: Me
 }
 type Mutation {
   createTweet(text: String!): Tweet
   updateTweet(_id: ID!, text: String): Tweet
   deleteTweet(_id: ID!): Status
-  signup(email: String!, fullName: String!, password: String!, avatar: String, username: String): User
-  login(email: String!, password: String!): User
+  signup(email: String!, fullName: String!, password: String!, avatar: String, username: String): Auth
+  login(email: String!, password: String!): Auth
 }
 schema {
   query: Query
